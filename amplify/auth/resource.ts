f50231ea98fb6d4c4cebcbd12b913c535d1b45da
System Administrator <root@Toms-MacBook-Pro.local>
2025-08-01 12:04:24 -0700

    add back

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,8 +9,7 @@ import { defineAuth } from '@aws-amplify/backend';
 
 export const auth = defineAuth({
   loginWith: {
-    email: true,
-    username: true
+    email: true
   },
   userAttributes: {
     email: {
