8fb754ce6524118b5a9d2d86aff3806e8a8ae81e
System Administrator <root@Toms-MacBook-Pro.local>
2025-08-01 11:43:52 -0700

    comment out

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
+//TW:import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 defineBackend({
-  auth,
+  //TW:auth,
   data,
 });
