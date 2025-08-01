983feae3fa1f3226cabbc503abee418e66ea5e99
Tom Withrow <tomwithrow@Toms-MacBook-Pro.local>
2025-08-01 10:55:01 -0700

    Comment out defineAuth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,6 +5,7 @@ import { defineAuth } from '@aws-amplify/backend';
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 
+/*
 export const auth = defineAuth({
   loginWith: {
     email: true,
@@ -17,5 +18,6 @@ export const auth = defineAuth({
   }
 
 });
+*/
 
 
